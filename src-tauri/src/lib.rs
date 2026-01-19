af771f72c826f3cd8d36e6670275ac15eabcd4ec
Abraham Escalante <aeklant@gmail.com>
2026-01-18 20:20:35 -0500

    DEV: add a User state to the app

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,10 +1,13 @@
+use std::sync::Mutex;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![
-            get_user,
-            login,
-        ])
+        .invoke_handler(tauri::generate_handler![get_user, login])
+        .manage(Mutex::new(User {
+            name: String::from(""),
+            password: String::from(""),
+        }))
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
